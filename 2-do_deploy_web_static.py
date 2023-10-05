b97b9f13fc80cb71a7e8933847fc2dc880f12c66
riyo3350g <yechouben@gamail.com>
2023-10-05 16:34:26 +0100

    Task 2 try

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,47 +3,46 @@
 that distributes an archive to your web servers,
 using the function do_deploy"""
 from fabric.api import *
-from os.path import *
-from datetime import datetime
+from os.path import exists
 env.hosts = ['52.3.255.32', '18.208.120.88']
 
-@task
-def do_pack():
-    """ method doc
-        sudo fab -f 1-pack_web_static.py do_pack
-    """
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
 
-
-@task
 def do_deploy(archive_path):
-    """ method doc
-        fab -f 2-do_deploy_web_static.py do_deploy:
-        archive_path=versions/web_static_20231004201306.tgz
-        -i ~/.ssh/id_rsa -u ubuntu
-    """
+    """func that distributes an archive to your web servers"""
     try:
-        if not exists(archive_path):
+        if exists(archive_path) is False:
             return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
-        return True
+            # Extract relevant information from the archive path
+            # Extract the filename from the path
+            file_name = archive_path.split("/")[-1]
+            # Extract filename without extension
+            no_ext_file = file_name.split(".")[0]
+            # Target directory path
+            no_ext_path = "/data/web_static/releases/" + no_ext_file
+            # Upload the archive to the /tmp/ directory on the remote server
+            put(archive_path, "/tmp/")
+
+            # Create the target directory on the remote server
+            run("sudo mkdir -p {}".format(no_ext_path))
+
+            # Extract the archive contents into the target directory
+            run("sudo tar -xzf /tmp/{} -C {}".format(file_name, no_ext_path))
+
+            # Remove the uploaded archive from the /tmp/ directory
+            run("sudo rm /tmp/{}".format(file_name))
+
+            # Move the extracted files and directories to the target directory
+            run("sudo mv {}/web_static/* {}".format(no_ext_path, no_ext_path))
+
+            # Remove the redundant web_static subdirectory
+            run("sudo rm -rf {}/web_static".format(no_ext_path))
+
+            # Remove the existing symbolic link to /data/web_static/current
+            run("sudo rm -rf /data/web_static/current")
+
+            # Create a new symbolic link to the deployed content
+            run("sudo ln -s {} /data/web_static/current".format(no_ext_path))
+            print("New version deployed!")
+            return True  # Deployment was successful
     except Exception:
-        return False
+        return False  # Deployment failed
