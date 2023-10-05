6a58c1943db76b2397e2326476a53a25e1495275
riyo3350g <yechouben@gamail.com>
2023-10-05 15:23:13 +0100

    Task 2 try uo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,9 +4,24 @@ that distributes an archive to your web servers,
 using the function do_deploy"""
 from fabric.api import *
 from os.path import exists
+from datetime import datetime
 env.hosts = ['52.3.255.32', '18.208.120.88']
 
 
+@task
+def do_pack():
+    """create an archive of the web_static folder"""
+    local("mkdir -p versions")
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(current_date)
+    res = local("tar -cvzf {} web_static".format(file_name))
+    if res.succeeded:
+        return file_name
+    else:
+        return None
+
+
+@task
 def do_deploy(archive_path):
     """func that distributes an archive to your web servers"""
     if exists(archive_path) is False:
@@ -21,42 +36,29 @@ def do_deploy(archive_path):
         no_ext_path = "/data/web_static/releases/" + no_ext_file
 
         # Upload the archive to the /tmp/ directory on the remote server
-        if put(archive_path, "/tmp/").failed is True:
-            return False
+        put(archive_path, "/tmp/")
 
         # Create the target directory on the remote server
-        if run("mkdir -p /data/web_static/releases/{}/".
-               format(no_ext_file)).failed is True:
-            return False
+        run("sudo mkdir -p {}".format(no_ext_path))
 
         # Extract the archive contents into the target directory
-        if run("sudo tar -xzf /tmp/{} -C {}".format(file_name, no_ext_path)).failed\
-           is True:
-            return False
+        run("sudo tar -xzf /tmp/{} -C {}".format(file_name, no_ext_path))
 
         # Remove the uploaded archive from the /tmp/ directory
-        if run("rm /tmp/{}".format(file_name)).failed is True:
-            return False
+        run("sudo rm /tmp/{}".format(file_name))
 
         # Move the extracted files and directories to the target directory
-        if run("sudo mv {}/web_static/* {}".format(no_ext_path, no_ext_path)).failed\
-           is True:
-            return False
+        run("sudo mv {}/web_static/* {}".format(no_ext_path, no_ext_path))
 
         # Remove the redundant web_static subdirectory
-        if run("rm -rf {}/web_static".format(no_ext_path)).failed is True:
-            return False
+        run("sudo rm -rf {}/web_static".format(no_ext_path))
 
         # Remove the existing symbolic link to /data/web_static/current
-        if run("rm -rf /data/web_static/current").failed is True:
-            return False
+        run("sudo rm -rf /data/web_static/current")
 
         # Create a new symbolic link to the deployed content as the new version
-        if run("ln -s {} /data/web_static/current".format(no_ext_path)).failed\
-           is True:
-            return False
-        
+        run("sudo ln -s {} /data/web_static/current".format(no_ext_path))
         print("New version deployed!")
-        return True  # Deployment successful
-    except Exception as e:
+        return True  # Deployment was successful
+    except Exception:
         return False  # Deployment failed
