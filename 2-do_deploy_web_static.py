474dd30c60c5c29d2cd2f155ebade28dcc382375
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-10-05 21:24:57 +0100

    Task 2 add

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,29 +6,21 @@ env.hosts = ['52.3.255.32', '18.208.120.88']
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to your web servers """
-    if not exists(archive_path):
+    """func: distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
-        # Uncompress the archive to the folder
         file_name = archive_path.split("/")[-1]
-        # file_name = file_name.split(".")[0]
-        no_ext_path = file_name.split(".")[0]
-        # no_ext_path = file_name
+        no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
-        # run("mkdir -p /data/web_static/releases/{}/".format(no_ext_path))
-        run("mkdir -p {}{}/".format(path, no_ext_path))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file_name,
-                                               path, no_ext_path))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, no_ext_path))
-        run("rm -rf {}{}/web_static".format(path, no_ext_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path,
-                                                          no_ext_path))
-        print("New version deployed!")
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
