90c277315ffbefca8d304896da77879cecccf1c2
riyo3350g <yechouben@gamail.com>
2023-10-05 19:00:33 +0100

    Task 2 added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Fabric script that distributes an archive to your web servers"""
 from fabric.api import *
-from os import path
+from os.path import exists
 env.hosts = ['52.3.255.32', '18.208.120.88']
 
 
 def do_deploy(archive_path):
     """ distributes an archive to your web servers """
-    if not path.exists(archive_path):
+    if not exists(archive_path):
         return False
     try:
         # Upload the archive to the /tmp/ directory of the web server
